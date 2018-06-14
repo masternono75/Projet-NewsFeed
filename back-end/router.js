261689501ffdbda39c60efcafa99997165f7f2ac
Arnaud AGBO <agbo.arnaud@yahoo.fr>
2018-06-14 11:01:06 +0200

    affichage Json back end

--- back-end/router.js
+++ back-end/router.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const axios = require('axios')
 const router = express.Router()
 
 router.route('/').get(function(request, response) {
     response.send('Hello World')
+})
+
+router.route('/news').get(function(request, response) {
+    response.send('Hd')
+})
+
+router.route('/comments').get((request, response) => {
+  var request = axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=f67d4d9879bd486a84fafe2e6a0b5dce')
+        .then((httpResponse) => response.send(httpResponse.data))
 })
 
 module.exports = router
